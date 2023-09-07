624a19eb0e7f5b6fedeccd4577a60dd7d5067375
Shelf <mohamedshelfwork@gmail.com>
2023-09-07 21:55:13 +0300

    Implemented Social media links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import InstagramIcon from './assets/icons/InstgramIcon'
+import TwitterIcon from './assets/icons/TwitterIcon'
 import NavigationBar from './components/NavigationBar'
 import PrimaryHeading from './components/common/PrimaryHeading'
 import SecondaryHeading from './components/common/SecondaryHeading'
@@ -16,6 +18,13 @@ function App() {
           <PrimaryHeading content={<span className="text-xl sm:text-3xl md:text-4xl lg:text-7xl">Be prepared for the <br /> Mountains and beyond!</span>} />
           <span className="animate-bounce"> <a href="#content" className="drop-shadow-custom">scroll down </a> <span className="text-3xl">↓</span> </span>
         </article>
+
+        {/* Socila Media Links Section */}
+        <ul className="hidden md:flex gap-6 rotate-90 absolute left-4">
+          <li><a href="#!">Follow</a></li>
+          <li className="rotate-[-90deg]"><a href="#!" className="hover:underline"><TwitterIcon /></a></li>
+          <li className="rotate-[-90deg]"><a href="#!" className="hover:underline"><InstagramIcon /></a></li>
+        </ul>
 
       </section >
 
